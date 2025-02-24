4cf0454b990e94a2d9bfe15a896d52821038696d
Dadihans23 <Dadihans06@gmail.com>
2025-02-24 11:02:24 +0100

    24 02

--- lib/util/constant.dart
+++ lib/util/constant.dart
@@ -1,4 +1,4 @@
 class Constants {
-  // static const String adresse = "172.20.10.2";
-  static const String adresse = "30.30.2.225" ;
+  static const String adresse = "172.20.10.2";
+  // static const String adresse = "30.30.2.225" ;
 }
